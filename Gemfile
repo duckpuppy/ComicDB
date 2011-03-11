8ba995553f15f30b64dc1dd85dd38229dbb073d6
Patrick Aikens <paikens@gmail.com>
2011-03-10 21:55:19 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'sqlite3'
+
+# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
+# gem 'ruby-debug19'
+
+group :development do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'webrat'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comicdb::Application.config.secret_token = 'd87a6adeafddd447494132a6ab5a49a3f53e1faf74c28e4e03194ccd709cc67c8bd1b3cee216804bb64a7cbd48ab5f9d318456bb372b04afad416b994b804c4d'
